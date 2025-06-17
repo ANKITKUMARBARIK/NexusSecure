90e7031e62b5958db6fc4a1860e3b85d94ec7c52
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-06-17 19:08:48 +0530

    feat: implement caching for product operations with Redis

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,6 +2,11 @@ import asyncHandler from "../utils/asyncHandler.util.js";
 import ApiError from "../utils/ApiError.util.js";
 import ApiResponse from "../utils/ApiResponse.util.js";
 import Product from "../models/product.model.js";
+import {
+    getCache,
+    setCache,
+    delCache,
+} from "../services/redisCache.service.js";
 
 export const createProduct = asyncHandler(async (req, res) => {
     const { name, description, price, category, brand = "" } = req.body;
@@ -19,42 +24,66 @@ export const createProduct = asyncHandler(async (req, res) => {
         throw new ApiError(500, "product not created");
     }
 
+    await delCache("products:all"); // clear old cache after new product creation
+
     return res
         .status(201)
         .json(new ApiResponse(201, product, "product created successfully"));
 });
 
 export const getAllProducts = asyncHandler(async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
+    // gell all products with cache
+    const cachedProducts = await getCache("products:all");
+    if (cachedProducts) {
+        console.log("♦️ Serving from cache");
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    cachedProducts,
+                    "products fetched successfully from cache"
+                )
+            );
+    }
 
     const products = await Product.find({})
-        .skip(skip)
-        .limit(limit)
         .populate("createdBy", "username email")
         .select("-__v")
         .sort("-createdAt");
 
+    await setCache("products:all", products, 300); // cache for 5 min
+
     return res
         .status(200)
-        .json(
-            new ApiResponse(
-                200,
-                { page, limit, products },
-                "products fetched successfully with pagination"
-            )
-        );
+        .json(new ApiResponse(200, products, "products fetched successfully"));
 });
 
 export const getProductById = asyncHandler(async (req, res) => {
     const { id } = req.params;
+
+    // get single product with cache
+    const cachedProduct = await getCache(`product:${id}`);
+    if (cachedProduct) {
+        console.log("♦️ Single product from cache");
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    cachedProduct,
+                    "products fetched successfully from cache"
+                )
+            );
+    }
 
     const product = await Product.findById(id)
         .populate("createdBy", "username email")
         .select("-__v");
 
     if (!product) throw new ApiError(404, "Product not found");
+
+    await setCache(`product:${id}`, product, 300);
 
     return res
         .status(200)
@@ -72,6 +101,8 @@ export const updateProductById = asyncHandler(async (req, res) => {
         .select("-__v");
     if (!product) throw new ApiError(404, "product not found");
 
+    await delCache("products:all", `product:${id}`);
+
     return res
         .status(200)
         .json(new ApiResponse(200, product, "product updated successfully"));
@@ -84,6 +115,8 @@ export const deleteProductById = asyncHandler(async (req, res) => {
         .populate("createdBy", "username email")
         .select("-__v");
     if (!product) throw new ApiError(404, "product not found");
+
+    await delCache("products:all", `product:${id}`);
 
     return res
         .status(200)
